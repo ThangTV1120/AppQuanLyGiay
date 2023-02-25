285d67eeb8421488795bce3a2a9f7f5bb4563117
Thang <thangchien86@gmail.com>
2023-02-25 13:25:34 +0700

    Update

--- app/src/main/java/com/example/appquanlygiay/activity_quenMK.java
+++ app/src/main/java/com/example/appquanlygiay/activity_quenMK.java
@@ -2,7 +2,9 @@ package com.example.appquanlygiay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class activity_quenMK extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class activity_quenMK extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quen_mk);
+        Button Login=findViewById(R.id.btnQuayLaiQuenMK);
+
+    }
+    public void onclickQuayLaiMain(){
+        Intent intent=new Intent(activity_quenMK.this,Login.class);
+        startActivity(intent);
     }
 }
